974f23a76ea85b374531880dc1bcfda528bdb54c
49494 - Catarina Pedro <a49494@alunos.isel.pt>
2024-04-16 16:00:48 +0100

    Computer Vision: Started a function that converts an RGB image to the HSV color space.
    Pioneer: Translate comments, and fixed some errors in variables names

--- computer_vision/main.py
+++ computer_vision/main.py
@@ -1,5 +1,7 @@
 from PIL import Image
 from utils import in_file, out_file
+import cv2
+import numpy as np
 
 # To convert any color to its approximate gray level, you must first obtain its red, green, and blue primitives (from
 # the RGB scale). Then add 30% of the red plus 59% of the green plus 11% of the blue, regardless of the scale used.
@@ -32,7 +34,87 @@ def grayscale(colored):
     return img
 
 
+def convert_rgb_to_hsv(img):
+    """
+    Converts an RGB image to the HSV color space.
+
+    Args:
+        img (str): Path to the input RGB image file.
+
+    This function reads the input RGB image, resizes it to 25% of the original size, converts it to the HSV color space,
+    and displays both the original RGB image and the converted HSV image in separate windows.
+    """
+    # Read the input RGB image
+    image = cv2.imread(img)
+
+    # Resize the image to 25% of the original size
+    image = cv2.resize(image, None, fx=0.25, fy=0.25)
+
+    # Convert the image from RGB to HSV
+    image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    Ib, Ib2, x, y = detect_object(image_hsv[:,:,0])
+
+    cv2.imshow('Original', image)
+    cv2.imshow('HSV', image_hsv)
+    cv2.imshow('Thresholded', Ib * 255)
+    cv2.imshow('Processed', Ib2 * 255)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    return image_hsv
+
+
+def detect_object(I_H):
+    Th = 40
+    Ib = (I_H > Th).astype(np.uint8)
+
+    se = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (4, 4))
+    Ib2 = cv2.morphologyEx(Ib, cv2.MORPH_OPEN, se)
+    Ib2 = cv2.morphologyEx(Ib2, cv2.MORPH_CLOSE, se)
+
+    contours, _ = cv2.findContours(Ib2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if contours:  # Verifica se a lista de contornos não está vazia
+        contour = contours[0]  # Pega o primeiro contorno
+        x, y, _, _ = cv2.boundingRect(contour)
+    else:
+        x = -1
+        y = -1
+
+    return Ib, Ib2, x, y
+
+
+"""
+def detect_objects_hue_threshold(hsv_image, hue_threshold):
+    # Separando o canal de matiz (H)
+    hue_channel = hsv_image[:, :, 0]
+
+    # Aplicando um limiar para identificar objetos
+    objects_mask = hue_channel > hue_threshold
+
+    return objects_mask
+
+
+# Definindo um limiar para a componente H (matiz)
+hue_threshold = 100
+
+# Detectando objetos com base no limiar da componente H
+objects_mask = detect_objects_hue_threshold(convert_rgb_to_hsv(in_file('coke_can.jpg')), hue_threshold)
+
+# Convertendo a máscara para tipo uint8 para exibição
+objects_mask = objects_mask.astype(np.uint8) * 255
+
+# Exibindo a máscara resultante
+cv2.imshow('Objects Mask', objects_mask)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+"""
+
+
 if __name__ == '__main__':
+    """
     coke_can = Image.open(in_file('coke_can.jpg'))
     bw_coke_can = grayscale(coke_can)
     bw_coke_can.save(out_file('coke_can.jpg'))
+    """
+    convert_rgb_to_hsv(in_file('yogurt.jpg'))
--- pioneer2_rs232_interface/ers.py
+++ pioneer2_rs232_interface/ers.py
@@ -2,69 +2,64 @@
 
 # Define import path
 import sys
-
-from command import Command
-from pioneer2_rs232_interface.sonars import update_sonar_info, print_sonar_info
-
-sys.path.append("./serial_communication")
-sys.path.append("./serial_communication/communication_protocol")
 import queue
-import time
-
+from command import Command
+from sonars import update_sonar_info, print_sonar_info
 from serial_communication.serial_communication import SerialCommunication
 from tests.timer import *
 
+sys.path.append("./serial_communication")
+sys.path.append("./serial_communication/communication_protocol")
+
 
 class ERS:
-    """ Class que permite comunicar com o robô Pioneer2 através de uma ligação série."""
+    """ Class that allows communication with the Pioneer2 robot through a serial connection. """
 
     def __init__(self, port, baudrate):
 
         self.__running_flag = None
         self.sip_info = None
         self.__command = None
-        self.verdadeiro = False
+        self.true = False
 
-        # Iniciar comunicação série
+        # Start serial communication
         self.__start_serial_connection(port, baudrate)
 
-        # Criar queue que vai conter os comandos a serem executados
+        # Create commands queue
         self.__commands_queue = queue.Queue()
 
         self.__execute_commands_queue = queue.Queue()
 
-        # Estabelecer ligação com o robô caso a ligação série seja iniciada
+        # Establish communication with the robot if the serial connection is started.
         if self.__serial_communication.is_connected():
-            # Estabelecer comunicação com o robô
             self.__establish_communication()
 
     def __start_serial_connection(self, port, baudrate):
         self.__serial_communication = SerialCommunication(port, baudrate)
 
     def __establish_communication(self):
-        """Sincronizar comunicação com o robô e iniciar os servidores e motores do robô."""
+        """ Synchronize communication with the robot and start the robot's servers and motors. """
         print("Pioneer2 RS-232 Interface - Establishing Connection")
 
-        # Enviar os pacotes de sincronização e obter as respostas a cada um
+        # Send synchronization packets and obtain the responses for each one.
         self.__send_command('SYNC0')
         self.__send_command('SYNC1')
         self.__send_command('SYNC2')
 
-        # Iniciar os servidores do robô
+        # Starts the controller
         self.__send_command('OPEN')
 
-
-        # Pedir o SIP de configuração
+        # Request configuration SIP
         self.__send_command('CONFIG')
 
-        # Ligar os motores do robô
+        # Enables the motors
         self.__send_command('ENABLE', 1)
 
-        # Reset da origem do robô
+        # RResets server to 0,0,0 origin
         self.__send_command('SETO')
 
     def add_console_command(self, command):
-        """Adiciona um comando à fila de comandos da consola."""
+        """Add a command to the command queue."""
         self.__commands_queue.put(command)
 
     def __process_sip(self):
@@ -72,38 +67,39 @@ class ERS:
 
         if sip_info_aux is not None:
             self.sip_info = sip_info_aux
-        else:
-            print("SIP não disponível")
-            #self.sip_info = None
+        # else:
+        # print("unavailable SIP ")
+        # self.sip_info = None
+
     def __process_command(self, command):
         print('Command received:' + command.comando + ' ' + str(command.args))
 
-        # Printar último SIP
+        # Print last SIP
         if command.comando == 'S':
             print(self.sip_info)
 
-        # Se o comando for para desligar a interface
+        # If the command is to turn off the interface
         elif command.comando == 'EXIT':
             self.turn_off()
             self.__running_flag = False
 
-        # Caso contrário, e se a comunicação série estiver ativa, tentar enviar o comando ao robô
+        # Otherwise, if the serial communication is active, attempt to send the command to the robot
         elif self.__serial_communication.is_connected():
             self.__send_command(command.comando, command.args)
 
-    def __process_sucessive_commands(self):
-        if self.sip_info is None and self.__command is None:  # 1º comando
+    def __process_successive_commands(self):
+        if self.sip_info is None and self.__command is None:  # 1st command
             self.__command = self.__commands_queue.get()
-            print("Executar 1º Comando")
+            print("execute the first command")
             self.__process_command(self.__command)
         else:
             if self.sip_info is not None:
-                if self.sip_info['motor_status']:  # 1ª fase de acabar
+                if self.sip_info['motor_status']:  # 1st phase of completion
                     self.__command = None
-                if not self.sip_info['motor_status'] and self.__command is None: # 2ª fase de acabar
+                if not self.sip_info['motor_status'] and self.__command is None:  # 2nd phase of completion
                     self.__command = self.__commands_queue.get()
-                #if self.__command is not None:
-                    print("Executar 2º Comando")
+                    # if self.__command is not None:
+                    print("execute the second command")
                     self.__process_command(self.__command)
 
     def __send_command(self, command, arg=None):
@@ -111,81 +107,78 @@ class ERS:
 
     def turn_off(self):
         if self.__serial_communication.is_connected():
-            # Desligar os motores do robô
+            # Disables the motors
             self.__send_command('ENABLE', 0)
 
-            # Terminar ligação com o robô
+            # Close server and client connection
             self.__send_command('CLOSE')
 
-            # Terminar ligação série
+            # Terminate serial connection
             self.__serial_communication.disconnect()
 
     def run(self):
         print("Pioneer2 RS-232 Interface - Running")
         self.__running_flag = True
 
-        # Tempo inicial
-        tempo_init = tempo_fin = datetime.now().timestamp()
+        # Initial time
+        tempo_init = final_time = datetime.now().timestamp()
 
-        # Tempo Pulse inicial
-        tempo_pulse_inicial = datetime.now().timestamp()
-        tempo_pulse_final = datetime.now().timestamp()
+        # Initial and final pulse time
+        initial_pulse_time, final_pulse_time = datetime.now().timestamp()
         sonars = []
         while self.__commands_queue.qsize() > 0 or self.__command is not None:
-            #print("Tamanho:", self.__commands_queue.qsize())
+            # print("size:", self.__commands_queue.qsize())
 
-            if self.__serial_communication.check_sip_availibility() and (tempo_fin - tempo_init > 0.100):
+            if self.__serial_communication.check_sip_availibility() and (final_time - tempo_init > 0.100):
                 tempo_init = datetime.now().timestamp()
                 self.__process_sip()
 
             if self.sip_info is not None:
                 sonar_info = self.sip_info['sonars']
-                updated_sonars = update_sonar_info(sonar_info,sonars)
+                updated_sonars = update_sonar_info(sonar_info, sonars)
                 print_sonar_info(updated_sonars)
 
-            self.__process_sucessive_commands()
+            self.__process_successive_commands()
 
-            # Manter robô acordado
-            if self.__serial_communication.is_connected() and (tempo_pulse_final - tempo_pulse_inicial > 1.500):
-                tempo_pulse_inicial = datetime.now().timestamp()
+            # Keep the robot awake
+            if self.__serial_communication.is_connected() and (final_pulse_time - initial_pulse_time > 1.500):
+                initial_pulse_time = datetime.now().timestamp()
                 print("Pulse")
                 self.__serial_communication.send_command('PULSE')
 
-            # Calcular tempos para criar um setinterval
-            # Tempo final 'loop'
-            tempo_fin = datetime.now().timestamp()
-            tempo_pulse_final = datetime.now().timestamp()
+            # Calculate times to create a set_interval
+            final_time = datetime.now().timestamp()
+            final_pulse_time = datetime.now().timestamp()
 
         print("Pioneer2 RS-232 Interface - Shutdown")
 
 
 def current_position(pioneer):
-    print("Posição em X:", pioneer.sip_info['x_pos'])
-    print("Posição em Y:", pioneer.sip_info['y_pos'])
-    print("Posição Heading:", pioneer.sip_info['th_pos'])
+    print("x pos:", pioneer.sip_info['x_pos'])
+    print("y pos:", pioneer.sip_info['y_pos'])
+    print("Heading pos:", pioneer.sip_info['th_pos'])
 
 
 if __name__ == '__main__':
-    pioneer2 = ERS('COM5', 9600)
+    pioneer2 = ERS('COM10', 9600)
     try:
-        #pioneer2.turn_off()
-        #pioneer2.add_console_command(Command('MOVE', 6000))
-        pioneer2.add_console_command(Command('MOVE', 100))
-        pioneer2.add_console_command(Command('POLLING', "\001\002\003\004\005\006\007\010"))
+        # pioneer2.turn_off()
+        # pioneer2.add_console_command(Command('MOVE', 6000))
+        pioneer2.add_console_command(Command('MOVE', 1000))
+        pioneer2.add_console_command(Command('SONAR', 1))
+        # pioneer2.add_console_command(Command('POLLING', "\001\002\003\004\005\006\007\010"))
         pioneer2.add_console_command(Command('MOVE', 20000))
         pioneer2.add_console_command(Command('MOVE', 100))
 
-
-
-
         # pioneer2.add_console_command(Command('HEAD', 90))
         # pioneer2.add_console_command(Command('SETO', None))
-        #pioneer2.add_console_command(Command('SONAR', 0))
-        #pioneer2.add_console_command(Command('BUMP_STALL', 0))
+        # pioneer2.add_console_command(Command('SONAR', 0))
+        # pioneer2.add_console_command(Command('BUMP_STALL', 0))
         # pioneer2.add_console_command(Command('EXIT', None))
         pioneer2.run()
         pioneer2.turn_off()
         current_position(pioneer2)
-    except:
-        print("Erro na execução")
+
+    except Exception as e:
+        print("Error during execution:", e)
         pioneer2.turn_off()
