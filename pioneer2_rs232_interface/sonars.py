20bfc7f9e61750974f6652a266bdf56bcf719252
48355 - Filipa Machado <a48355@alunos.isel.pt>
2024-04-17 13:02:45 +0100

    sonars a printar bem tentativa 2

--- pioneer2_rs232_interface/sonars.py
+++ pioneer2_rs232_interface/sonars.py
@@ -1,5 +1,19 @@
-import time
 from datetime import datetime
+from enum import Enum
+
+
+class Direction(Enum):
+    BACKWARD = -1,
+    STAY = 0
+    LEFT = 1,
+    RIGHT = 2,
+    FORWARD = 3
+
+
+class Rotate:
+    def __init__(self, direction, sonar_id):
+        self.direction = direction
+        self.sonar_id = sonar_id
 
 
 class SonarInfo:
@@ -27,4 +41,33 @@ def update_sonar_info(sonars, arr_sonar):
 
 def print_sonar_info(sonars):
     for sonarInfo in sonars:
-            sonarInfo.display_info()
+        sonarInfo.display_info()
+
+
+def detect_obj(arr_sonar):
+    for sonar in arr_sonar:
+        if sonar.distance < 50 and sonar.id in (2, 3, 4, 5):
+            return "STOP"
+
+
+
+
+if __name__ == "__main__":
+    sonars_data = [
+        {'sonar_number': 0, 'sonar_range': 100},
+        {'sonar_number': 1, 'sonar_range': 200},
+
+        {'sonar_number': 2, 'sonar_range': 300},
+        {'sonar_number': 3, 'sonar_range': 20},
+        {'sonar_number': 4, 'sonar_range': 500},
+        {'sonar_number': 5, 'sonar_range': 600},
+
+        {'sonar_number': 6, 'sonar_range': 700},
+        {'sonar_number': 7, 'sonar_range': 800}
+    ]
+
+    arr_sonar = []
+    create_sonar(arr_sonar)
+    update_sonar_info(sonars_data, arr_sonar)
+    direction = detect_obj(arr_sonar)
+    print(direction)
